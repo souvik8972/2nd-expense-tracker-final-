a52afbccef7147e96f79580dc6d391fa87d051e9
souvik das <souvik8582@gmail.com>
2023-12-26 10:48:38 +0530

    .env

--- util/db.js
+++ util/db.js
@@ -4,9 +4,10 @@ const DB_NAME=process.env.DB_NAME
 const DB_PASSWORD=process.env.DB_PASSWORD
 const DB_USER=process.env.DB_USER
 const DB_HOST_NAME=process.env.DB_HOST_NAME
+const DB_DIALECT=process.env.DB_DIALECT
 
 const sequelize = new Sequelize(DB_NAME,DB_USER,DB_PASSWORD,{
-    dialect:'mysql',
+    dialect:DB_DIALECT,
     host:DB_HOST_NAME,
     logging:false
 });
